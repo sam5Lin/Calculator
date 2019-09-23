050647384a4dce70de08885a10e127eeb0f0db6b
sam5Lin <1150050235@qq.com>
2019-09-23 11:19:07 +0800

    第一次提交

---
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.calculator;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public  void myclick(View v){
+        Toast.makeText(getApplicationContext(), "button is clicked",  Toast.LENGTH_SHORT).show();
+    }
+}
