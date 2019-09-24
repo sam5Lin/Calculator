f3f4d5220fe11e16312da00777800e330a71b5b2
sam5Lin <1150050235@qq.com>
2019-09-24 11:04:43 +0800

    完成0不可除

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.calculator;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,12 +11,21 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     private TextView tv;
     private StringBuffer text;
+    private Button btn;
+    private int flag;//设置flag判断输入特殊符号的情况
+                    // 0:不能输入特殊符号 1：可以输入特殊符号 2：可以更改最后一个特殊符号
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tv = (TextView) findViewById(R.id.textField);
         text = new StringBuffer(tv.getText());
+
+        findViewById(R.id.zero).setOnClickListener(this);
         findViewById(R.id.one).setOnClickListener(this);
         findViewById(R.id.two).setOnClickListener(this);
         findViewById(R.id.three).setOnClickListener(this);
@@ -33,65 +43,76 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         findViewById(R.id.sub).setOnClickListener(this);
         findViewById(R.id.equal).setOnClickListener(this);
 
+        flag = 0;
     }
 
     @Override
     public void onClick(View v){
-        switch (v.getId()){
+        switch(v.getId()){
+
             case R.id.one:
-                text.append("1");
-                tv.setText(text);
-                break;
             case R.id.two:
-                text.append("2");
-                tv.setText(text);
-                break;
             case R.id.three:
-                text.append("3");
-                tv.setText(text);
-                break;
             case R.id.four:
-                text.append("4");
-                tv.setText(text);
-                break;
             case R.id.five:
-                text.append("5");
-                tv.setText(text);
-                break;
             case R.id.six:
-                text.append("6");
-                tv.setText(text);
-                break;
             case R.id.seven:
-                text.append("7");
-                tv.setText(text);
-                break;
             case R.id.eight:
-                text.append("8");
-                tv.setText(text);
-                break;
             case R.id.nine:
-                text.append("9");
+                btn = findViewById(v.getId());
+                text.append(btn.getText());
                 tv.setText(text);
+                flag = 1;   //输入数字之后可以继续加入符号
                 break;
+            case R.id.zero:
+                if(text.charAt(text.length() - 1) == '÷'){
+                    text.append("0");
+                    text.append("\n");
+                    text.append("不能除于0");
+                    tv.setText(text);
+                    text.delete(0,text.length());
+                    flag = 0;
+                    break;
+                }
+                btn = findViewById(v.getId());
+                text.append(btn.getText());
+                tv.setText(text);
+                flag = 1;   //输入数字之后可以继续加入符号
+                break;
+            case R.id.point:
             case R.id.add:
-                text.append("+");
-                tv.setText(text);
-                break;
-            case R.id.sub:
-                text.append("-");
-                tv.setText(text);
-                break;
-            case R.id.mul:
-                text.append("x");
-                tv.setText(text);
-                break;
+            case R.id.sub: //减号可以是第一个 当text为空时，可以有减号
             case R.id.div:
-                text.append("÷");
+            case R.id.mul:
+                btn = findViewById(v.getId());
+                if(flag == 1){ //可以输入特殊符号
+                    text.append(btn.getText());
+                    tv.setText(text);
+                    flag = 2;
+                }
+                else if(flag == 2){
+                    text.deleteCharAt(text.length() - 1);
+                    text.append(btn.getText());
+                    tv.setText(text);
+                    flag = 2;
+                }
+
+                break;
+            case R.id.AC:
+                text.delete(0,text.length());
                 tv.setText(text);
+                System.out.println(text);
+                break;
+            case R.id.del:
+                System.out.println(text);
+                if(text.length() > 0){
+                    text.deleteCharAt(text.length()-1);
+                    tv.setText(text);
+
+                }
                 break;
             case R.id.equal:
-                //modify
+                //
                 break;
                 default:break;
         }
